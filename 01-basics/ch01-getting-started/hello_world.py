eda72886db081fa51962ee842bc7d58881a1521f
mindeleven <mindeleven@posteo.de>
2024-07-23 13:53:10 +0200

    First edit on GrapheneOS

--- 01-basics/ch01-getting-started/hello_world.py
+++ 01-basics/ch01-getting-started/hello_world.py
@@ -1,1 +1,7 @@
-print("Hello Python world!")
+# /home/projects/python/python-crash-course/01-basics/ch01-getting-started
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course"
+print(message)
