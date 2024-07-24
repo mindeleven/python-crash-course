d8b4a85e660a277e2db8a9d8e8b08913480fec40
mindeleven <mindeleven@posteo.de>
2024-07-24 09:23:35 +0200

    Small string examples

---
+++ 01_basics/ch02_variables_and_simple_data_types/name.py
@@ -0,0 +1,13 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(full_name.title())
+
+message = f"Good morning, {full_name.title()}!\n\tWhat a lovely day :)"
+print(message)
