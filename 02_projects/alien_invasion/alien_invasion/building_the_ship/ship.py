df174e45e652fd6d3fdec1b25bc3a69dcf3d119b
mindeleven <mindeleven@posteo.de>
2024-07-26 10:51:01 +0200

    Defining the ships speed in settings and adjusting it

--- 02_projects/alien_invasion/alien_invasion/building_the_ship/ship.py
+++ 02_projects/alien_invasion/alien_invasion/building_the_ship/ship.py
@@ -7,6 +7,8 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
         # screen gets assigned to an attribute of ship
         self.screen_rect = ai_game.screen.get_rect()
 
@@ -19,6 +21,10 @@ class Ship:
         # ship image gets centered horizontally and aligned to the bottom
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # store a float for the ships exact horizontal position
+        # converting the value of self.rect.x to a float
+        self.x = float(self.rect.x)
+
         # movement flag; start with a ship that's not moving
         self.moving_right = False # ship will be motionless
         self.moving_left = False
@@ -27,10 +33,18 @@ class Ship:
         """Update the ships position based on the movement flag."""
         # moves the ship to the right if moving_right is True
         if self.moving_right:
-            self.rect.x += 1
+            # self.rect.x += 1
+            # update the ships x value, not the rect
+            self.x += self.settings.ship_speed
         # moves the ship to the left if moving_left is True
         if self.moving_left:
-            self.rect.x -= 1
+            # self.rect.x -= 1
+            # update the ships x value, not the rect
+            self.x -= self.settings.ship_speed
+        
+        # update rect object from self.x
+        # only the integer position will be assigned to self.rect.x
+        self.rect.x = self.x
 
 
     def blitme(self):
